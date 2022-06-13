5b519938db5bf93c69f63fbda1af4e7ceec6e814
Smeyend <Smeyend@gmail.com>
2022-06-13 20:04:37 +0700

    Class DBWorker add

--- src/Study.java
+++ src/Study.java
@@ -4,16 +4,16 @@ import java.sql.Statement;
 
 public class Study {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         chosenGay chosen = new chosenGay(21, 67);
         System.out.println(chosen.probability());
         DBWorker worker = new DBWorker();
         String query = "select * from users";
         try {
             Statement statement = worker.getConnection().createStatement();
-            ResultSet resultSet =statement.executeQuery(query);
+            ResultSet resultSet = statement.executeQuery(query);
 
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 int id = resultSet.getInt(1);
                 System.out.println(id);
             }
