55d334866dc357fecec5d09e7861e813f2fcb2a0
Smeyend <Smeyend@gmail.com>
2022-06-13 20:03:36 +0700

    Class DBWorker add

---
+++ src/DBWorker.java
@@ -0,0 +1,25 @@
+import java.sql.*;
+
+
+public class DBWorker {
+    private final String URL = "jdbc:mysql://localhost:3306/mydbtest?useUnicode=true&useSSL=true&useJDBCCompliantTimezoneShift=true" +
+            "&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private final String USERNAME = "root";
+    private final String PASSWORD = "root";
+    private Connection connection;
+
+    public DBWorker() {
+        try  {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
+
+
+
--- src/Study.java
+++ src/Study.java
@@ -1,27 +1,26 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Study {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest?useUnicode=true&useSSL=true&useJDBCCompliantTimezoneShift=true" +
-            "&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
     public static void main(String[] args){
         chosenGay chosen = new chosenGay(21, 67);
         System.out.println(chosen.probability());
-
-
-        Connection connection;
+        DBWorker worker = new DBWorker();
+        String query = "select * from users";
         try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            if (!connection.isClosed()) {
-                System.out.println("123");
+            Statement statement = worker.getConnection().createStatement();
+            ResultSet resultSet =statement.executeQuery(query);
+
+            while (resultSet.next()){
+                int id = resultSet.getInt(1);
+                System.out.println(id);
             }
-            connection.close();
         } catch (SQLException e) {
-            System.out.println("1234");
+            e.printStackTrace();
         }
+
+
     }
 }
