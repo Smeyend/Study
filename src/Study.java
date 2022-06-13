bbc3e0c321e93231d347b6d9eff89a8d46da44ab
Smeyend <Smeyend@gmail.com>
2022-06-13 17:01:11 +0700

    Add JDBC driver

--- src/Study.java
+++ src/Study.java
@@ -1,7 +1,28 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Study {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest?useUnicode=true&useSSL=true&useJDBCCompliantTimezoneShift=true" +
+            "&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
     public static void main(String[] args){
         chosenGay chosen = new chosenGay(21, 67);
         System.out.println(chosen.probability());
+
+
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("123");
+            }
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("1234");
+        }
     }
 }
